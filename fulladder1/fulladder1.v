140bbd404824d9ea750ec153449dae72167f43fc
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-05-26 20:08:30 +0530

    Add files via upload

---
+++ fulladder1/fulladder1.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:45:21 05/26/2023 
+// Design Name: 
+// Module Name:    fulladder1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module fulladder1(sum,carry,a,b,c);
+input a,b,c;
+output sum,carry;
+assign sum=a^b^c;
+assign carry=(a&b)|(b&c)|(c&a);
+endmodule
+//gate level coding:
+//module gate(a,b,c,sum,carry);
+//input a,b,c;
+//output sum,carry;
+//wire w1,w2,w3;
+//xor x1(sum,a,b,c);
+//and a1(w1,a,b);
+//and a2(w2,b,c);
+//and a3(w3,a,c);
+//or o1(carry,w1,w2,w3);
+//endmodule
+//
+//
+//Behavioural modeling
+//module fulladder(sum,carry,a,b,c);
+//input a,b,c;
+//output sum,carry;
+//reg sum,carry;
+//always@(a,b,c)
+//begin
+//case({a,b,c})
+/*3'b000:{sum,carry}=2'b00;
+3'b001:{sum,carry}=2'b10;
+3'b010:{sum,carry}=2'b10;
+3'b011:{sum,carry}=2'b01;
+3'b100:{sum,carry}=2'b10;
+3'b101:{sum,carry}=2'b01;
+3'b110:{sum,carry}=2'b01;
+3'b111:{sum,carry}=2'b01;
+endcase
+end
+endmodule
+*/
+
+
+
---
+++ fulladder1/isim/fulladder1tb_isim_beh.exe.sim/work/m_00000000004052508103_2447624706.c
@@ -0,0 +1,256 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "time=$g,a=%b,b=%b,c=%b,sum=%b,carry=%b";
+static const char *ng1 = "D:/100 days/fulladder1/fulladder1tb.v";
+static int ng2[] = {0, 0};
+static int ng3[] = {1, 0};
+
+void Monitor_45_1(char *);
+void Monitor_45_1(char *);
+
+
+static void Monitor_45_1_Func(char *t0)
+{
+    char t1[16];
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+    char *t9;
+    char *t10;
+    char *t11;
+    char *t12;
+    char *t13;
+    char *t14;
+
+LAB0:    t2 = xsi_vlog_time(t1, 1000.0000000000000, 1000.0000000000000);
+    t3 = (t0 + 1608);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t0 + 1768);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    t9 = (t0 + 1928);
+    t10 = (t9 + 56U);
+    t11 = *((char **)t10);
+    t12 = (t0 + 1048U);
+    t13 = *((char **)t12);
+    t12 = (t0 + 1208U);
+    t14 = *((char **)t12);
+    xsi_vlogfile_write(1, 0, 3, ng0, 7, t0, (char)118, t1, 64, (char)118, t5, 1, (char)118, t8, 1, (char)118, t11, 1, (char)118, t13, 1, (char)118, t14, 1);
+
+LAB1:    return;
+}
+
+static void Initial_44_0(char *t0)
+{
+
+LAB0:    xsi_set_current_line(44, ng1);
+
+LAB2:    xsi_set_current_line(45, ng1);
+    Monitor_45_1(t0);
+
+LAB1:    return;
+}
+
+static void Initial_48_2(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+
+LAB0:    t1 = (t0 + 3096U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(48, ng1);
+
+LAB4:    xsi_set_current_line(50, ng1);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(51, ng1);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(52, ng1);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(55, ng1);
+    t2 = (t0 + 2904);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB5;
+
+LAB1:    return;
+LAB5:    xsi_set_current_line(55, ng1);
+    t3 = ((char*)((ng2)));
+    t4 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t4, t3, 0, 0, 1);
+    xsi_set_current_line(55, ng1);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(55, ng1);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(56, ng1);
+    t2 = (t0 + 2904);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB6;
+    goto LAB1;
+
+LAB6:    xsi_set_current_line(56, ng1);
+    t3 = ((char*)((ng2)));
+    t4 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t4, t3, 0, 0, 1);
+    xsi_set_current_line(56, ng1);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(56, ng1);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(57, ng1);
+    t2 = (t0 + 2904);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB7;
+    goto LAB1;
+
+LAB7:    xsi_set_current_line(57, ng1);
+    t3 = ((char*)((ng2)));
+    t4 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t4, t3, 0, 0, 1);
+    xsi_set_current_line(57, ng1);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(57, ng1);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(58, ng1);
+    t2 = (t0 + 2904);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB8;
+    goto LAB1;
+
+LAB8:    xsi_set_current_line(58, ng1);
+    t3 = ((char*)((ng3)));
+    t4 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t4, t3, 0, 0, 1);
+    xsi_set_current_line(58, ng1);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(58, ng1);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(59, ng1);
+    t2 = (t0 + 2904);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB9;
+    goto LAB1;
+
+LAB9:    xsi_set_current_line(59, ng1);
+    t3 = ((char*)((ng3)));
+    t4 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t4, t3, 0, 0, 1);
+    xsi_set_current_line(59, ng1);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(59, ng1);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(60, ng1);
+    t2 = (t0 + 2904);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB10;
+    goto LAB1;
+
+LAB10:    xsi_set_current_line(60, ng1);
+    t3 = ((char*)((ng3)));
+    t4 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t4, t3, 0, 0, 1);
+    xsi_set_current_line(60, ng1);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(60, ng1);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(61, ng1);
+    t2 = (t0 + 2904);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB11;
+    goto LAB1;
+
+LAB11:    xsi_set_current_line(61, ng1);
+    t3 = ((char*)((ng3)));
+    t4 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t4, t3, 0, 0, 1);
+    xsi_set_current_line(61, ng1);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(61, ng1);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    goto LAB1;
+
+}
+
+void Monitor_45_1(char *t0)
+{
+    char *t1;
+    char *t2;
+
+LAB0:    t1 = (t0 + 3152);
+    t2 = (t0 + 3664);
+    xsi_vlogfile_monitor((void *)Monitor_45_1_Func, t1, t2);
+
+LAB1:    return;
+}
+
+
+extern void work_m_00000000004052508103_2447624706_init()
+{
+	static char *pe[] = {(void *)Initial_44_0,(void *)Initial_48_2,(void *)Monitor_45_1};
+	xsi_register_didat("work_m_00000000004052508103_2447624706", "isim/fulladder1tb_isim_beh.exe.sim/work/m_00000000004052508103_2447624706.didat");
+	xsi_register_executes(pe);
+}
