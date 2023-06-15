55192d72d3b26fea0dfc9a501e17425ad052219a
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-06-15 21:54:49 +0530

    Add files via upload

---
+++ DAY22-SR flip flop asynchronous/srasy.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:13:58 06/15/2023 
+// Design Name: 
+// Module Name:    srasy 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module srasy(clk,reset,s,r,q);
+input clk,reset,s,r;
+output q;
+reg  q;
+always@(posedge clk,reset)
+begin
+if(reset==1)
+q=0;
+else
+case({s,r})
+2'b00:q=q;
+2'b01:q =0;
+2'b10:q =1;
+2'b11:q =1'bz;
+endcase
+end
+endmodule
---
+++ DAY22-SR flip flop asynchronous/srasytb.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   21:17:30 06/15/2023
+// Design Name:   srasy
+// Module Name:   D:/100 days/srasy/srasytb.v
+// Project Name:  srasy
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: srasy
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module srasytb;
+
+	// Inputs
+	reg clk;
+	reg reset;
+	reg s;
+	reg r;
+
+	// Outputs
+	wire q;
+
+	// Instantiate the Unit Under Test (UUT)
+	srasy uut (
+		.clk(clk), 
+		.reset(reset), 
+		.s(s), 
+		.r(r), 
+		.q(q)
+	);
+
+	initial begin
+	$monitor("clk=%b,reset=%b,s=%b,r=%b,q=%b",clk,reset,s,r,q);
+	end
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		reset = 1;
+
+		// Wait 100 ns for global reset to finish
+		#100;s=0;r=0;reset=0;
+		#100;s=0;r=1;
+		#100;s=1;r=0;
+		#100;s=1;r=1;
+		
+        
+		// Add stimulus here
+
+	end
+	always
+	#30 clk=~clk;
+      
+endmodule
+
