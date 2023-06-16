f7122e7abf001f4a14c400ed1b4b71b5040cb5ea
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-06-16 22:12:55 +0530

    Add files via upload

---
+++ DAY23-JK flip flop asynchronous/jkasy.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:41:19 06/16/2023 
+// Design Name: 
+// Module Name:    jkasy 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module jkasy(clk,reset,j,k,q);
+input clk,reset,j,k;
+output q;
+reg q;
+always@(posedge clk,reset)
+begin
+if(reset==1)
+q=1'b0;
+else
+case({j,k})
+2'b00:q=q;
+2'b01:q=1'b0;
+2'b10:q=1'b1;
+2'b11:q=~q;
+endcase
+end
+endmodule
---
+++ DAY23-JK flip flop asynchronous/jkasytb.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   20:43:25 06/16/2023
+// Design Name:   jkasy
+// Module Name:   D:/100 days/jkasy/jkasytb.v
+// Project Name:  jkasy
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: jkasy
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module jkasytb;
+
+	// Inputs
+	reg clk;
+	reg reset;
+	reg j;
+	reg k;
+
+	// Outputs
+	wire q;
+
+	// Instantiate the Unit Under Test (UUT)
+	jkasy uut (
+		.clk(clk), 
+		.reset(reset), 
+		.j(j), 
+		.k(k), 
+		.q(q)
+	);
+
+	initial begin
+	$monitor("clk=%b,reset=%b,j=%b,k=%b,q=%b",clk,reset,j,k,q);
+	end
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		reset = 1;
+		// Wait 100 ns for global reset to finish
+		#100;reset = 0;j = 0;k = 0;
+		#100;j = 0;k = 1;
+		#100;j = 1;k = 0;
+		#100;j = 1;k = 1;  
+		// Add stimulus here
+
+	end
+	always
+	#50 clk=~clk;
+      
+endmodule
+
+
