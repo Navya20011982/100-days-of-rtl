a5ed6e30204b72e982ad89d54921e774d03f5bf9
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-06-21 23:37:26 +0530

    Add files via upload

---
+++ Day27-2 bit  down  counter synchronous/down.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:00:07 06/21/2023 
+// Design Name: 
+// Module Name:    down 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module down(reset,clk,count_out);
+input reset,clk;
+output [1:0]count_out;
+reg [1:0]count_down=2'b11;
+always@(posedge clk)
+begin
+if(reset)
+count_down=2'b11;
+else
+count_down=count_down-2'b01;
+end
+assign count_out=count_down;
+endmodule
---
+++ Day27-2 bit  down  counter synchronous/downtb.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   23:03:18 06/21/2023
+// Design Name:   down
+// Module Name:   D:/100 days/down/downtb.v
+// Project Name:  down
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: down
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module downtb;
+
+	// Inputs
+	reg reset;
+	reg clk;
+
+	// Outputs
+	wire [1:0] count_out;
+
+	// Instantiate the Unit Under Test (UUT)
+	down uut (
+		.reset(reset), 
+		.clk(clk), 
+		.count_out(count_out)
+	);
+	initial begin
+	$monitor("clk=%b,reset=%b,count_out=%b",clk,reset,count_out);
+	end
+
+	initial begin
+		// Initialize Inputs
+		reset = 1;
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+		#200;reset = 0;
+        
+		// Add stimulus here
+
+	end
+	always 
+	#5 clk=~clk; 
+endmodule
+
