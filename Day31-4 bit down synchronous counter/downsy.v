03f72f102d2eec11eea8d45f3ee755e9d3dbfaec
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-06-25 21:25:58 +0530

    Add files via upload

---
+++ Day31-4 bit down synchronous counter/downsy.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:35:13 06/25/2023 
+// Design Name: 
+// Module Name:    downsy 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module downsy(reset,clk,count_out);
+input reset,clk;
+output [3:0]count_out;
+reg [3:0]count_down=4'b1111;
+always@(posedge clk)
+begin
+if(reset)
+count_down=4'b1111;
+else
+count_down=count_down-4'b0001;
+end
+assign count_out=count_down;
+endmodule
+
+
---
+++ Day31-4 bit down synchronous counter/downsytb.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   20:41:36 06/25/2023
+// Design Name:   downsy
+// Module Name:   D:/100 days/fourbitdownsynchronous/downsytb.v
+// Project Name:  fourbitdownsynchronous
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: downsy
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module downsytb;
+
+	// Inputs
+	reg reset;
+	reg clk;
+
+	// Outputs
+	wire [3:0] count_out;
+
+	// Instantiate the Unit Under Test (UUT)
+	downsy uut (
+		.reset(reset), 
+		.clk(clk), 
+		.count_out(count_out)
+	);
+	initial begin
+	$monitor("clk=%b,reset=%b,count_out=%b",clk,reset,count_out);
+	end
+
+	initial begin
+		// Initialize Inputs
+		reset = 1;
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+	  #200;reset = 0;
+        
+		// Add stimulus here
+
+	end
+	always 
+	#5 clk=~clk; 
+      
+endmodule
+
