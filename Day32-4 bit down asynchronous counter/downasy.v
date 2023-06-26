f762ded605e79473996a4cab9875934b1bc247bd
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-06-26 23:06:16 +0530

    Add files via upload

---
+++ Day32-4 bit down asynchronous counter/downasy.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:43:10 06/22/2023 
+// Design Name: 
+// Module Name:    downasy 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module downasy(reset,clk,count_out);
+input reset,clk;
+output [1:0]count_out;
+reg [1:0]count_down=2'b11;
+always@(posedge clk,reset)
+begin
+if(reset)
+count_down=2'b11;
+else
+count_down=count_down-2'b01;
+end
+assign count_out=count_down;
+endmodule
