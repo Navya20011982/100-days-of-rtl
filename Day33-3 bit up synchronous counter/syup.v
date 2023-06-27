007e245dc0a56f7d938def5efb674edfcb881332
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-06-27 22:38:11 +0530

    Add files via upload

---
+++ Day33-3 bit up synchronous counter/syup.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:12:28 06/27/2023 
+// Design Name: 
+// Module Name:    syup 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module syup(reset,clk,count_out);
+input reset,clk;
+output [2:0]count_out;
+reg [2:0]count_up=3'b000;
+always@(posedge clk)
+begin
+if(reset)
+count_up=3'b000;
+else
+count_up=count_up+3'b001;
+end
+assign count_out=count_up;
+endmodule
+   
---
+++ Day33-3 bit up synchronous counter/upsytb.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:18:06 06/27/2023
+// Design Name:   syup
+// Module Name:   D:/100 days/twobitsynch/upsytb.v
+// Project Name:  twobitsynch
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: syup
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module upsytb;
+
+	// Inputs
+	reg reset;
+	reg clk;
+
+	// Outputs
+	wire [2:0] count_out;
+
+	// Instantiate the Unit Under Test (UUT)
+	syup uut (
+		.reset(reset), 
+		.clk(clk), 
+		.count_out(count_out)
+	);
+	initial begin
+	$monitor("clk=%b,reset=%b,count_out=%b",clk,reset,count_out);
+	end
+
+	initial begin
+		// Initialize Inputs
+		reset = 1;
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+	  #50;reset = 0;
+        
+		// Add stimulus here
+
+	end
+	always 
+	#5 clk=~clk; 
+      
+endmodule
+
+
+	
+
