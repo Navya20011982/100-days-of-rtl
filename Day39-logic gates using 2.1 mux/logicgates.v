c10e4fc5fb9b8a9da0462befddfe45213f449584
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-11-04 19:57:51 +0530

    Add files via upload

---
+++ Day39-logic gates using 2.1 mux/logicgates.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:37:23 11/03/2023 
+// Design Name: 
+// Module Name:    logicgates 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module logicgates(a,b,outputand,outputor,outputnot);
+   input a,b;
+   output outputand,outputor,outputnot;
+   reg outputand,outputor,outputnot;
+always @(a,b)
+     begin
+       if(a==1'b0)
+         begin
+           outputand = 1'b0;
+           outputor = b;
+           outputnot = 1'b1;
+         end
+       else
+         begin
+            outputand = b;
+            outputor = 1'b1;
+            outputnot = 1'b0;
+         end
+  end
+endmodule
---
+++ Day39-logic gates using 2.1 mux/logicgatestb.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   17:44:14 11/03/2023
+// Design Name:   logicgates
+// Module Name:   D:/100 days/logicgates/logicgatestb.v
+// Project Name:  logicgates
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: logicgates
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module logicgatestb;
+
+	// Inputs
+	reg a;
+	reg b;
+
+	// Outputs
+	wire outputand;
+	wire outputor;
+	wire outputnot;
+
+	// Instantiate the Unit Under Test (UUT)
+	logicgates uut (
+		.a(a), 
+		.b(b), 
+		.outputand(outputand), 
+		.outputor(outputor), 
+		.outputnot(outputnot)
+	);
+
+	initial begin
+		// Initialize Inputs
+		#100 a = 1'b0; b=1'b0;
+       #100 a = 1'b0; b=1'b1;
+      #100  a = 1'b1; b=1'b0;
+      #100 a = 1'b1; b=1'b1;
+		end
+      
+endmodule
+
