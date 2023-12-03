f9471cba0a04fba4b69447cdd5e8d996fd3a3f52
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-12-04 00:42:56 +0530

    Add files via upload

---
+++ Day45-SR Flip flop asynchronous/sr4.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:12:06 12/04/2023 
+// Design Name: 
+// Module Name:    sr4 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module sr4(clk,reset,s,r,q);
+input clk,reset,s,r;
+output q;
+reg q;
+always@(posedge clk or negedge reset)
+begin
+if(reset==1)
+q=0;
+else
+case({s,r})
+2'b00:q=q;
+2'b01:q=1'b0;
+2'b10:q=1'b1;
+2'b11:q=1'bz;
+endcase
+end
+
+endmodule
---
+++ Day45-SR Flip flop asynchronous/sr4tb.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   00:15:21 12/04/2023
+// Design Name:   sr4
+// Module Name:   D:/100 days/sr4/sr4tb.v
+// Project Name:  sr4
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: sr4
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module sr4tb;
+
+	// Inputs
+	reg clk;
+	reg reset;
+	reg s;
+	reg r;
+
+	// Outputs
+	wire q;
+
+	// Instantiate the Unit Under Test (UUT)
+	sr4 uut (
+		.clk(clk), 
+		.reset(reset), 
+		.s(s), 
+		.r(r), 
+		.q(q)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		reset = 1;
+		
+
+		// Wait 100 ns for global reset to finish
+		#100  reset=0;s = 0;r = 0;
+		#100  s=0;r=1;
+		#100 s=1;r=0;
+		#100 s=1;r=1;
+		
+        
+		// Add stimulus here
+
+	end
+	always #30 clk=~clk;
+      
+
+      
+endmodule
+
