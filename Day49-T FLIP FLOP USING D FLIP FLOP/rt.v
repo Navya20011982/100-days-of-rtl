09113ed74fc2094167490aa77123894c1e3d2e86
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-12-09 12:14:50 +0530

    Add files via upload

---
+++ Day49-T FLIP FLOP USING D FLIP FLOP/rt.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:09:00 12/08/2023 
+// Design Name: 
+// Module Name:    rt 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module rt(clk,rst,T,Q,Q_bar);
+input clk,rst,T;
+output Q,Q_bar;
+wire w1;
+
+assign w1 = T^Q;
+
+d_ff DF(clk,rst,w1,Q,Q_bar);
+
+endmodule
+
+module d_ff(clk,rst,D,Q,Q_bar);
+input clk,rst, D;
+output reg Q;
+output Q_bar;
+always @(posedge clk) begin
+if(!rst)
+  Q <= D;
+else
+  Q <= 1'b0;
+end
+assign Q_bar = ~Q;
+endmodule
+  
---
+++ Day49-T FLIP FLOP USING D FLIP FLOP/rttb.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:33:19 12/08/2023
+// Design Name:   rt
+// Module Name:   D:/100 days/rt/rttb.v
+// Project Name:  rt
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: rt
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module rttb;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg T;
+
+	// Outputs
+	wire Q;
+	wire Q_bar;
+
+	// Instantiate the Unit Under Test (UUT)
+	rt uut (
+		.clk(clk), 
+		.rst(rst), 
+		.T(T), 
+		.Q(Q), 
+		.Q_bar(Q_bar)
+	);
+
+	initial begin
+	clk = 1'b1; 
+forever #5 clk = ~clk;
+end
+
+initial begin
+rst = 1'b1;#5;
+rst = 1'b0;
+end
+initial begin
+T = 1'b0;
+ forever #50 T = ~T;
+
+	end
+      
+endmodule
+
