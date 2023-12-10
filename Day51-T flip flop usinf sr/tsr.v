c695df0b686ee29995364971faf71a22842b51ab
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-12-10 20:39:32 +0530

    Add files via upload

---
+++ Day51-T flip flop usinf sr/tsr.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:13:20 12/10/2023 
+// Design Name: 
+// Module Name:    tsr 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module tsr(clk,rst,T,Q,Q_bar);
+input clk,rst,T;
+output Q,Q_bar;
+wire w1,w2;
+
+assign w1 = T & ~Q;
+assign w2 = T & Q;
+
+sr_ff SRF(clk,rst,w1,w2,Q,Q_bar);
+
+endmodule
+
+module sr_ff(clk,rst,S,R, q,q_bar);
+input clk,rst,S,R;
+output reg q;
+output q_bar;
+always@(posedge clk)
+begin
+if(!rst) begin
+  case({S,R})
+    2'b00: q <= q;
+  2'b01: q <= 1'b0;
+  2'b10: q <= 1'b1;
+  2'b11: q <= 1'bx;
+  endcase
+end
+else
+q <= 1'b0;
+end
+
+assign q_bar = ~q;
+endmodule
---
+++ Day51-T flip flop usinf sr/tsrtb.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   20:18:55 12/10/2023
+// Design Name:   tsr
+// Module Name:   D:/100 days/tsr/tsrtb.v
+// Project Name:  tsr
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: tsr
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module tsrtb;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg T;
+
+	// Outputs
+	wire Q;
+	wire Q_bar;
+
+	// Instantiate the Unit Under Test (UUT)
+	tsr uut (
+		.clk(clk), 
+		.rst(rst), 
+		.T(T), 
+		.Q(Q), 
+		.Q_bar(Q_bar)
+	);
+
+	initial begin
+		clk = 1'b1; 
+forever #5 clk = ~clk;
+end
+
+initial begin
+rst = 1'b1;#5;
+rst = 1'b0;
+end
+initial begin
+T = 1'b0;
+ forever #20 T = ~T;
+
+	end
+      
+endmodule
+
