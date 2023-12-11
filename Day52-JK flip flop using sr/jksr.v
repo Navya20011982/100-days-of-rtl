780de25e6a384a9035cd859e95744f7c79a5159d
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-12-11 22:06:11 +0530

    Add files via upload

---
+++ Day52-JK flip flop using sr/jksr.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:46:30 12/11/2023 
+// Design Name: 
+// Module Name:    jksr 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module jksr(clk,rst,J,K,Q,Q_bar);
+  input clk, rst,J,K;
+  output Q,Q_bar;
+  wire w1,w2;
+  assign w1 = J & ~Q;
+  assign w2 = K & Q;
+  sr_ff SRF(clk,rst, w1,w2,Q,Q_bar);
+endmodule
+
+module sr_ff(clk,rst,S,R, Q,Q_bar);
+input clk,rst,S,R;
+output reg Q;
+output Q_bar;
+always@(posedge clk)
+begin
+if(!rst) begin
+  case({S,R})
+    2'b00: Q <= Q;
+	2'b01: Q <= 1'b0;
+	2'b10: Q <= 1'b1;
+	2'b11: Q <= 1'bx;
+  endcase
+  end
+else
+  Q = 1'b0;
+end
+
+assign Q_bar = ~Q;
+
+endmodule
---
+++ Day52-JK flip flop using sr/jksrtb.v
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   21:49:27 12/11/2023
+// Design Name:   jksr
+// Module Name:   D:/100 days/jksr/jksrtb.v
+// Project Name:  jksr
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: jksr
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module jksrtb;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg J;
+	reg K;
+
+	// Outputs
+	wire Q;
+	wire Q_bar;
+
+	// Instantiate the Unit Under Test (UUT)
+	jksr uut (
+		.clk(clk), 
+		.rst(rst), 
+		.J(J), 
+		.K(K), 
+		.Q(Q), 
+		.Q_bar(Q_bar)
+	);
+
+	initial begin
+	 clk=1'b1;
+     forever #5 clk = ~clk;  
+end
+initial begin
+  rst=1'b1;#5;
+  rst = 1'b0;  
+end
+initial begin
+  J = 1'b0; K = 1'b0; #10; //Initial memory
+  
+  J = 1'b0; K = 1'b1; #10; //Reset State
+  J = 1'b0; K = 1'b0; #10; //Memory State
+  
+  J = 1'b1; K = 1'b0; #10; //Set State
+  J = 1'b0; K = 1'b0; #10; //Memory State
+  
+  J = 1'b1; K = 1'b1; #10; //Toggle Stat
+		
+	end
+      
+endmodule
+
