258defe33da7637dd4690da55a92426f83113596
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-12-12 22:54:23 +0530

    Add files via upload

---
+++ Day53-JK flip flop using t/jkt.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:41:35 12/12/2023 
+// Design Name: 
+// Module Name:    jkt 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module jkt(clk,rst,J,K,Q,Q_bar);
+  input clk, rst,J,K;
+  output Q,Q_bar;
+  wire w1,w2;
+  
+  assign w1 = (K & Q) | (J & ~Q);
+  
+  t_ff TF(clk,rst, w1,Q,Q_bar);
+  
+endmodule
+
+module t_ff(
+    input clk,rst,T,
+    output reg Q, output Q_bar
+    );
+    always@(posedge clk)
+          begin
+            if(rst)
+               Q <= 1'b0;
+            else
+                begin
+                   if(T)
+                    Q<= ~Q;
+                   else
+                    Q<= Q;                     
+                end
+          end
+		  assign Q_bar = ~Q;
+endmodule
