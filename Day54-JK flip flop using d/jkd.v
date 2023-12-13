49c2647cc4651b8bf514bdb870806dcef6092d27
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-12-13 23:06:17 +0530

    Add files via upload

---
+++ Day54-JK flip flop using d/jkd.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:46:22 12/13/2023 
+// Design Name: 
+// Module Name:    jkd 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module jkd(clk,rst,J,K,Q,Q_bar);
+  input clk, rst,J,K;
+  output Q,Q_bar;
+  wire w1;
+  
+  assign w1 = (~K & Q) | (J & ~Q);
+  
+  d_ff DF(clk,rst, w1,Q,Q_bar);
+  
+endmodule
+
+module d_ff(clk,rst,D,Q,Q_bar);
+input clk,rst, D;
+output reg Q;
+output Q_bar;
+always @(posedge clk) begin
+if(!rst)
+  Q <= D;
+else
+  Q <= 1'b0;
+end
+assign Q_bar = ~Q;
+endmodule
---
+++ Day54-JK flip flop using d/jkdtb.v
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:47:25 12/13/2023
+// Design Name:   jkd
+// Module Name:   D:/100 days/jkd/jkdtb.v
+// Project Name:  jkd
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: jkd
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module jkdtb;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg J;
+	reg K;
+
+	// Outputs
+	wire Q;
+	wire Q_bar;
+
+	// Instantiate the Unit Under Test (UUT)
+	jkd uut (
+		.clk(clk), 
+		.rst(rst), 
+		.J(J), 
+		.K(K), 
+		.Q(Q), 
+		.Q_bar(Q_bar)
+	);
+
+	initial begin
+	 clk=1'b1;
+     forever #5 clk = ~clk;  
+end
+initial begin
+  rst=1'b1;#5;
+  rst = 1'b0;  
+end
+initial begin
+  J = 1'b0; K = 1'b0; #10; //Initial memory
+  
+  J = 1'b0; K = 1'b1; #10; //Reset State
+  J = 1'b0; K = 1'b0; #10; //Memory State
+  
+  J = 1'b1; K = 1'b0; #10; //Set State
+  J = 1'b0; K = 1'b0; #10; //Memory State
+  
+  J = 1'b1; K = 1'b1; #10; //Toggle State
+end
+
+      
+endmodule
+
