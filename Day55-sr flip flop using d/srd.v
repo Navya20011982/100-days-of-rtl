7e240d4288dbd11f5836b396cf9f97c69632a745
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-12-14 22:43:39 +0530

    Add files via upload

---
+++ Day55-sr flip flop using d/srd.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:33:08 12/14/2023 
+// Design Name: 
+// Module Name:    srd 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module srd(clk,S,R, Q,Q_bar);
+   input clk,S,R;
+   output Q,Q_bar;
+   wire w1;
+   
+   assign w1 = (S)|(Q & (~R));
+   d_ff DF(clk, w1, Q,Q_bar);
+   
+endmodule
+
+module d_ff(clk,D,Q,Q_bar);
+input clk, D;
+output reg Q;
+output Q_bar;
+always @(posedge clk) begin
+Q <= D;
+end
+assign Q_bar = ~Q;
+endmodule
---
+++ Day55-sr flip flop using d/srdtb.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:34:48 12/14/2023
+// Design Name:   srd
+// Module Name:   D:/100 days/srd/srdtb.v
+// Project Name:  srd
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: srd
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module srdtb;
+
+	// Inputs
+	reg clk;
+	reg S;
+	reg R;
+
+	// Outputs
+	wire Q;
+	wire Q_bar;
+
+	// Instantiate the Unit Under Test (UUT)
+	srd uut (
+		.clk(clk), 
+		.S(S), 
+		.R(R), 
+		.Q(Q), 
+		.Q_bar(Q_bar)
+	);
+
+	initial begin
+	 clk=1'b1;
+     forever #5 clk = ~clk;  
+end 
+
+initial begin
+  S = 1'b0; R = 1'b0; #10; //Initial memory
+  
+  S = 1'b0; R = 1'b1; #10; //Reset State
+  S = 1'b0; R = 1'b0; #10; //Memory State
+  
+  S = 1'b1; R = 1'b0; #10; //Set State
+  S = 1'b0; R = 1'b0; #10; //Memory State
+  
+  S = 1'b1; R = 1'b1; #10; //Forbidden State
+  S = 1'b0; R = 1'b0; #10; //Memory State
+end
+      
+endmodule
+
