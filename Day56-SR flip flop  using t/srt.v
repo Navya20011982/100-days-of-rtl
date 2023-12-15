30c9b1a8fdc8f28a215bd95a914b11708c71c5e4
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-12-15 22:05:49 +0530

    Add files via upload

---
+++ Day56-SR flip flop  using t/srt.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:53:32 12/15/2023 
+// Design Name: 
+// Module Name:    srt 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module srt(clk,rst,S,R, Q,Q_bar);
+   input clk,rst,S,R;
+   output Q,Q_bar;
+   wire w1;
+   
+   assign w1 = (R&Q) | (S&Q_bar);
+   t_ff TF(clk, rst,w1, Q,Q_bar);
+   
+endmodule
+
+module t_ff(
+    input clk,rst,T,
+    output reg Q, output Q_bar
+    );
+    always@(posedge clk)
+          begin
+            if(rst)
+               Q <= 1'b0;
+            else
+                begin
+                   if(T)
+                    Q<= ~Q;
+                   else
+                    Q<= Q;                     
+                end
+          end
+		  assign Q_bar = ~Q;
+endmodule
---
+++ Day56-SR flip flop  using t/srttb.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   21:54:56 12/15/2023
+// Design Name:   srt
+// Module Name:   D:/100 days/srt/srttb.v
+// Project Name:  srt
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: srt
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module srttb;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg S;
+	reg R;
+
+	// Outputs
+	wire Q;
+	wire Q_bar;
+
+	// Instantiate the Unit Under Test (UUT)
+	srt uut (
+		.clk(clk), 
+		.rst(rst), 
+		.S(S), 
+		.R(R), 
+		.Q(Q), 
+		.Q_bar(Q_bar)
+	);
+
+	initial begin
+	clk=1'b1;
+     forever #5 clk = ~clk;  
+end 
+
+initial begin
+rst = 1'b1;#5;
+rst = 1'b0;
+end
+
+initial begin
+  S = 1'b0; R = 1'b0; #10; //Initial memory
+  
+  S = 1'b0; R = 1'b1; #10; //Reset State
+  S = 1'b0; R = 1'b0; #10; //Memory State
+  
+  S = 1'b1; R = 1'b0; #10; //Set State
+  S = 1'b0; R = 1'b0; #10; //Memory State
+  
+  S = 1'b1; R = 1'b1; #10; //Forbidden State
+  S = 1'b0; R = 1'b0; #10; //Memory State
+end
+      
+endmodule
+
