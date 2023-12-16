2dd39a31c368fba6cd450d550ca6847d85f85933
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-12-16 21:33:16 +0530

    Add files via upload

---
+++ Day57-SR flip flop using jk/srjk.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:03:46 12/16/2023 
+// Design Name: 
+// Module Name:    srjk 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module srjk(clk,S,R, Q,Q_bar);
+   input clk,S,R;
+   output Q;
+   output Q_bar;
+   jk_ff JKF(clk,S,R, Q);
+   assign Q_bar = ~Q;
+  endmodule
+
+
+module jk_ff(clk,J,K, Q);  
+   input clk,J,K;
+   output reg Q;   
+  
+   always @(posedge clk)  
+   begin
+      case ({J,K})  
+         2'b00 :  Q <= Q;  
+         2'b01 :  Q <= 1'b0;  
+         2'b10 :  Q <= 1'b1;  
+         2'b11 :  Q <= ~Q;  
+      endcase  
+   end
+endmodule
---
+++ Day57-SR flip flop using jk/srjktb.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   21:05:16 12/16/2023
+// Design Name:   srjk
+// Module Name:   D:/100 days/srjk/srjktb.v
+// Project Name:  srjk
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: srjk
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module srjktb;
+
+	// Inputs
+	reg clk;
+	reg S;
+	reg R;
+
+	// Outputs
+	wire Q;
+	wire Q_bar;
+
+	// Instantiate the Unit Under Test (UUT)
+	srjk uut (
+		.clk(clk), 
+		.S(S), 
+		.R(R), 
+		.Q(Q), 
+		.Q_bar(Q_bar)
+	);
+
+	initial begin
+	clk=1'b1;
+     forever #5 clk = ~clk;  
+end 
+
+initial begin
+  S = 1'b0; R = 1'b0; #10; //Initial memory
+  
+  S = 1'b0; R = 1'b1; #10; //Reset State
+  S = 1'b0; R = 1'b0; #10; //Memory State
+  
+  S = 1'b1; R = 1'b0; #10; //Set State
+  S = 1'b0; R = 1'b0; #10; //Memory State
+  
+  S = 1'b1; R = 1'b1; #10; //Forbidden State
+  S = 1'b0; R = 1'b0; #10; //Memory State
+	end
+      
+endmodule
+
