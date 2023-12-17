f873a0f9a454e277d0231d3979bfe4469ce0589f
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-12-17 20:43:44 +0530

    Add files via upload

---
+++ Day58-D flip flop using  t/dt.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:29:18 12/17/2023 
+// Design Name: 
+// Module Name:    dt 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module dt(clk,rst,D,Q,Q_bar);
+input clk, rst, D;
+output Q,Q_bar;
+wire w1;
+
+assign w1 = (~D & Q) | (D & ~Q);
+t_ff TF(clk, rst,w1, Q,Q_bar );
+endmodule
+
+
+module t_ff(
+    input clk,rst,T,
+    output reg Q, output Q_bar
+    );
+    always@(posedge clk)
+          begin
+            if(rst)
+               Q <= 1'b0;
+            else
+                begin
+                   if(T)
+                    Q<= ~Q;
+                   else
+                    Q<= Q;                     
+                end
+          end
+		  assign Q_bar = ~Q;
+endmodule
---
+++ Day58-D flip flop using  t/dttb.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   20:31:03 12/17/2023
+// Design Name:   dt
+// Module Name:   D:/100 days/srd/dttb.v
+// Project Name:  srd
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: dt
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module dttb;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg D;
+
+	// Outputs
+	wire Q;
+	wire Q_bar;
+
+	// Instantiate the Unit Under Test (UUT)
+	dt uut (
+		.clk(clk), 
+		.rst(rst), 
+		.D(D), 
+		.Q(Q), 
+		.Q_bar(Q_bar)
+	);
+
+	initial begin
+clk = 1'b1; 
+forever #5 clk = ~clk;
+end
+
+initial begin
+rst = 1'b1; #5;
+rst = 1'b0;
+end
+
+initial begin
+D = 1'b0;
+ forever #10 D = ~D;
+end
+endmodule
+
