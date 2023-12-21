76c4a2de66f3cec863baedc986066a78082fcfa2
Navya20011982 <120656278+Navya20011982@users.noreply.github.com>
2023-12-21 22:34:42 +0530

    Add files via upload

---
+++ DAY 60 -D FLIP FLOP USING SR/dsr.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:18:37 12/21/2023 
+// Design Name: 
+// Module Name:    dsr 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module dsr(clk,D,Q,Q_bar);
+input clk, D;
+output Q,Q_bar;
+
+sr_ff SRF(clk, D,~D, Q,Q_bar );
+endmodule
+
+
+module sr_ff(clk,S,R, q,q_bar);
+input clk,S,R;
+output reg q;
+output q_bar;
+
+always@(posedge clk)
+begin
+  case({S,R})
+    2'b00: q <= q;
+  2'b01: q <= 1'b0;
+  2'b10: q <= 1'b1;
+  2'b11: q <= 1'bx;
+  endcase
+end
+
+assign q_bar = ~q;
+endmodule
---
+++ DAY 60 -D FLIP FLOP USING SR/dsrtb.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:25:07 12/21/2023
+// Design Name:   dsr
+// Module Name:   N:/100 days rtl/dsr/dsrtb.v
+// Project Name:  dsr
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: dsr
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module dsrtb;
+
+	// Inputs
+	reg clk;
+	reg D;
+
+	// Outputs
+	wire Q;
+	wire Q_bar;
+
+	// Instantiate the Unit Under Test (UUT)
+	dsr uut (
+		.clk(clk), 
+		.D(D), 
+		.Q(Q), 
+		.Q_bar(Q_bar)
+	);
+
+	initial begin
+	clk = 1'b0; 
+forever #10 clk = ~clk;
+end
+
+initial begin
+D = 1'b0;
+ forever #20 D = ~D;
+end
+      
+endmodule
+
